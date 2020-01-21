1ee14fc8e0c1276844b0072afd56763228bb27ba
이성준 <dhzm2aud@naver.com>
2020-01-21 17:50:28 +0900

    [add] Footer component를 추가
    issue : S02P12D110-107, S02P12D110-105
    story : S02P12D110-103
    
    - footer 컴포넌트 생성
    - app에 연결
    - git link 연결
    - w3css cdn 연결
    - font-awesome cnd 연결

---
+++ front/somebodyhelpme/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>&nbsp;
+    <i class="fa fa-instagram w3-hover-opacity"></i>&nbsp;
+    <i class="fa fa-github w3-hover-opacity"></i>&nbsp;
+    <i class="fa fa-gitlab w3-hover-opacity"></i>&nbsp;
+    <i class="fa fa-twitter w3-hover-opacity"></i>&nbsp;
+    <p class="w3-medium">
+      Git URL by
+      <a href="http://lab.ssafy.com/webmobile1-sub2/s02p12d110.git" target="_blank">Somebody Help MeL</a>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {
+    msg: String
+  },
+  methods:{
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
