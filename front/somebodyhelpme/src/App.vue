506f5eb9f32787874a047bd3732f9c86d42a1978
이성준 <dhzm2aud@naver.com>
2020-01-22 16:27:39 +0900

    [fix] 화면 컴포넌트 이름 중복
    - home의 화면이 중복으로 나오는 버그 수정
    - index의 cdn 링크 수정
    - 전체 스타일 추가

--- front/somebodyhelpme/src/App.vue
+++ front/somebodyhelpme/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
+  <div id="app" class="w3-light-grey w3-content" style="max-width:1600px">
     <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
-    <Sidebar></Sidebar>
-    <Home></Home>
+    <Sidebar class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;"></Sidebar>
+    <Home class="w3-main" style="margin-left:300px"></Home>
   </div>
 </template>
 
@@ -23,26 +23,14 @@ export default {
   }
 };
 </script>
-
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Raleway", sans-serif;
 }
 </style>
