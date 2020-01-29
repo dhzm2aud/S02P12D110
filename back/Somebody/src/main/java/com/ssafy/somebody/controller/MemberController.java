3cae865d6ec108b4103ca76a70f81da677f136a3
안경진 <akj0426@naver.com>
2020-01-29 12:18:48 +0900

    [fix] 회원 Controller 수정
    Issue : S02P12D110-123
    Story : S02P12D110-117
    
    - searchAllMember 기능 추가
    - 회원 컨트롤러 insertMemberInfo 삭제 (updateMember로 처리할것)
    - 테스트 출력코드 삭제
    - 코드 정렬

--- back/Somebody/src/main/java/com/ssafy/somebody/controller/MemberController.java
+++ back/Somebody/src/main/java/com/ssafy/somebody/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.ssafy.somebody.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.somebody.service.MemberService;
@@ -22,74 +23,68 @@ import com.ssafy.somebody.vo.Members;
 @RequestMapping("member")
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 public class MemberController {
-	
+
 	@Autowired
 	MemberService memberservice;
 
 	@GetMapping("/searchMember/{membersId}")
 	public ResponseEntity<BoolResult> searchMember(@PathVariable String membersId) throws Exception {
-		System.out.println("뜨뜬");
 		Members members = memberservice.searchMember(membersId);
 		BoolResult nr = new BoolResult();
 		if (members == null) {
 			nr.setState("fail");
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}else {
+			return new ResponseEntity<BoolResult>(HttpStatus.NO_CONTENT);
+		} else {
 			nr.setState("succ");
 		}
-		return new ResponseEntity (members, HttpStatus.OK);
+		return new ResponseEntity(members, HttpStatus.OK);
 	}
-	
+
+	@GetMapping("/searchAllMember")
+	public ResponseEntity<BoolResult> searchAllMember() throws Exception {
+		List<Members> memberslist = memberservice.searchAllMember();
+		BoolResult nr = new BoolResult();
+		nr.setState("succ");
+		return new ResponseEntity(memberslist, HttpStatus.OK);
+	}
+
 	@PostMapping("/insertMember")
-	public ResponseEntity<BoolResult> insertMember(@RequestBody Members member) throws Exception{
+	public ResponseEntity<BoolResult> insertMember(@RequestBody Members member) {
 		System.out.println(member.getMembersId());
 		boolean flag = memberservice.insertMember(member) > 0 ? true : false;
-		BoolResult nr=new BoolResult();
+		BoolResult nr = new BoolResult();
 		if (!flag) {
 			nr.setState("fail");
-		}else {
-		nr.setState("succ");
+		} else {
+			nr.setState("succ");
 		}
-		return new ResponseEntity<BoolResult>(nr,HttpStatus.OK);
+		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
 	}
-	
-	@PostMapping("/insertMemberInfo")
-	public ResponseEntity<BoolResult> insertMemberInfo(@RequestBody Members member) throws Exception{
-		System.out.println(member.getMembersId());
-		boolean flag = memberservice.updateMember(member) > 0 ? true : false;
-		BoolResult nr=new BoolResult();
-		if (!flag) {
-			nr.setState("fail");
-		}else {
-		nr.setState("succ");
-		}
-		return new ResponseEntity<BoolResult>(nr,HttpStatus.OK);
-	}
-	
+
 	@PutMapping("/updateMember")
-	public ResponseEntity<BoolResult> updateMember(@RequestBody Members member) throws Exception{
-		boolean flag =  memberservice.updateMember(member) > 0 ? true : false;
-		BoolResult nr=new BoolResult();
-		if(flag==false) {
+	public ResponseEntity<BoolResult> updateMember(@RequestBody Members member) throws Exception {
+		boolean flag = memberservice.updateMember(member) > 0 ? true : false;
+		BoolResult nr = new BoolResult();
+		if (flag == false) {
 			nr.setState("fail");
-		}else {
+		} else {
 			nr.setState("succ");
 		}
 		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
 	}
-	
-	@DeleteMapping("/deleteMember/{id}")
-	public ResponseEntity<BoolResult> deleteMember(@PathVariable String membersId) throws Exception{
+
+	@DeleteMapping("/deleteMember/{membersId}")
+	public ResponseEntity<BoolResult> deleteMember(@PathVariable String membersId) throws Exception {
 		boolean flag = memberservice.deleteMember(membersId) > 0 ? true : false;
-		BoolResult nr=new BoolResult();
+		BoolResult nr = new BoolResult();
 		if (!flag) {
 			nr.setState("fail");
-		}else {
+		} else {
 			nr.setState("succ");
 		}
 		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/checkPass") // = login
 	public ResponseEntity<BoolResult> checkPass(@RequestBody Members member) throws Exception {
 		Members members = memberservice.passCheck(member.getMembersId(), member.getPassword());
@@ -97,9 +92,9 @@ public class MemberController {
 		if (members == null) {
 			nr.setState("fail");
 			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}else {
+		} else {
 			nr.setState("succ");
 		}
-		return new ResponseEntity (nr, HttpStatus.OK);
+		return new ResponseEntity(nr, HttpStatus.OK);
 	}
 }
