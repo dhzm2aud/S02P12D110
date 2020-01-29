abbb9e07085f5fe9684bf928bb95e75a6351ae9a
신중호 <fantasque@naver.com>
2020-01-29 18:21:51 +0900

    [fix] 경매 참가 목록 UI 수정 Issue : S02P12D110-170 Story : S02P12D110-168
    - 참가한 경매 목록 UI(Auction.vue)
    - vue-carousel ^0.18.0 사용(package.json, package-lock.json 수정)

--- front/somebodyhelpme/src/components/home/Auction.vue
+++ front/somebodyhelpme/src/components/home/Auction.vue
@@ -20,6 +20,166 @@
             </li>
           </ul>
         </div>
+
+        <div class="w3-third w3-margin-bottom">
+          <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+            <img src="https://placehold.it/320" style="width:100%" class="w3-hover-opacity" />
+            <li class="w3-padding-16">헬로우 드림1</li>
+            <li class="w3-padding-16">블로그배포 2018 어워즈 수상! 크몽랭킹 1위! 카페/블로그 광고포스팅 팍팍 도와 드립니다</li>
+            <li class="w3-padding-16">
+              <i class="fa fa-star font-size-h6 color-dark-yellow-400" />
+              4.9
+              <h4 class="rate w3-show-inline-block">651개의 평가</h4>
+            </li>
+            <li class="w3-padding-16">
+              <h2>₩ 100,000</h2>
+            </li>
+            <li class="w3-light-grey w3-padding-24">
+              <button class="w3-button w3-teal w3-padding-large w3-hover-black">조회하기</button>
+            </li>
+          </ul>
+        </div>
+
+        <div class="w3-third w3-margin-bottom">
+          <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+            <img src="https://placehold.it/320" style="width:100%" class="w3-hover-opacity" />
+            <li class="w3-padding-16">헬로우 드림1</li>
+            <li class="w3-padding-16">블로그배포 2018 어워즈 수상! 크몽랭킹 1위! 카페/블로그 광고포스팅 팍팍 도와 드립니다</li>
+            <li class="w3-padding-16">
+              <i class="fa fa-star font-size-h6 color-dark-yellow-400" />
+              4.9
+              <h4 class="rate w3-show-inline-block">651개의 평가</h4>
+            </li>
+            <li class="w3-padding-16">
+              <h2>₩ 100,000</h2>
+            </li>
+            <li class="w3-light-grey w3-padding-24">
+              <button class="w3-button w3-teal w3-padding-large w3-hover-black">조회하기</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </slide>
+
+    <slide>
+      <div class="w3-row-padding">
+        <div class="w3-third w3-margin-bottom">
+          <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+            <img src="https://placehold.it/320" style="width:100%" class="w3-hover-opacity" />
+            <li class="w3-padding-16">헬로우 드림1</li>
+            <li class="w3-padding-16">블로그배포 2018 어워즈 수상! 크몽랭킹 1위! 카페/블로그 광고포스팅 팍팍 도와 드립니다</li>
+            <li class="w3-padding-16">
+              <i class="fa fa-star font-size-h6 color-dark-yellow-400" />
+              4.9
+              <h4 class="rate w3-show-inline-block">651개의 평가</h4>
+            </li>
+            <li class="w3-padding-16">
+              <h2>₩ 100,000</h2>
+            </li>
+            <li class="w3-light-grey w3-padding-24">
+              <button class="w3-button w3-teal w3-padding-large w3-hover-black">조회하기</button>
+            </li>
+          </ul>
+        </div>
+
+        <div class="w3-third w3-margin-bottom">
+          <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+            <img src="https://placehold.it/320" style="width:100%" class="w3-hover-opacity" />
+            <li class="w3-padding-16">헬로우 드림1</li>
+            <li class="w3-padding-16">블로그배포 2018 어워즈 수상! 크몽랭킹 1위! 카페/블로그 광고포스팅 팍팍 도와 드립니다</li>
+            <li class="w3-padding-16">
+              <i class="fa fa-star font-size-h6 color-dark-yellow-400" />
+              4.9
+              <h4 class="rate w3-show-inline-block">651개의 평가</h4>
+            </li>
+            <li class="w3-padding-16">
+              <h2>₩ 100,000</h2>
+            </li>
+            <li class="w3-light-grey w3-padding-24">
+              <button class="w3-button w3-teal w3-padding-large w3-hover-black">조회하기</button>
+            </li>
+          </ul>
+        </div>
+
+        <div class="w3-third w3-margin-bottom">
+          <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+            <img src="https://placehold.it/320" style="width:100%" class="w3-hover-opacity" />
+            <li class="w3-padding-16">헬로우 드림1</li>
+            <li class="w3-padding-16">블로그배포 2018 어워즈 수상! 크몽랭킹 1위! 카페/블로그 광고포스팅 팍팍 도와 드립니다</li>
+            <li class="w3-padding-16">
+              <i class="fa fa-star font-size-h6 color-dark-yellow-400" />
+              4.9
+              <h4 class="rate w3-show-inline-block">651개의 평가</h4>
+            </li>
+            <li class="w3-padding-16">
+              <h2>₩ 100,000</h2>
+            </li>
+            <li class="w3-light-grey w3-padding-24">
+              <button class="w3-button w3-teal w3-padding-large w3-hover-black">조회하기</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </slide>
+
+    <slide>
+      <div class="w3-row-padding">
+        <div class="w3-third w3-margin-bottom">
+          <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+            <img src="https://placehold.it/320" style="width:100%" class="w3-hover-opacity" />
+            <li class="w3-padding-16">헬로우 드림1</li>
+            <li class="w3-padding-16">블로그배포 2018 어워즈 수상! 크몽랭킹 1위! 카페/블로그 광고포스팅 팍팍 도와 드립니다</li>
+            <li class="w3-padding-16">
+              <i class="fa fa-star font-size-h6 color-dark-yellow-400" />
+              4.9
+              <h4 class="rate w3-show-inline-block">651개의 평가</h4>
+            </li>
+            <li class="w3-padding-16">
+              <h2>₩ 100,000</h2>
+            </li>
+            <li class="w3-light-grey w3-padding-24">
+              <button class="w3-button w3-teal w3-padding-large w3-hover-black">조회하기</button>
+            </li>
+          </ul>
+        </div>
+
+        <div class="w3-third w3-margin-bottom">
+          <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+            <img src="https://placehold.it/320" style="width:100%" class="w3-hover-opacity" />
+            <li class="w3-padding-16">헬로우 드림1</li>
+            <li class="w3-padding-16">블로그배포 2018 어워즈 수상! 크몽랭킹 1위! 카페/블로그 광고포스팅 팍팍 도와 드립니다</li>
+            <li class="w3-padding-16">
+              <i class="fa fa-star font-size-h6 color-dark-yellow-400" />
+              4.9
+              <h4 class="rate w3-show-inline-block">651개의 평가</h4>
+            </li>
+            <li class="w3-padding-16">
+              <h2>₩ 100,000</h2>
+            </li>
+            <li class="w3-light-grey w3-padding-24">
+              <button class="w3-button w3-teal w3-padding-large w3-hover-black">조회하기</button>
+            </li>
+          </ul>
+        </div>
+
+        <div class="w3-third w3-margin-bottom">
+          <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+            <img src="https://placehold.it/320" style="width:100%" class="w3-hover-opacity" />
+            <li class="w3-padding-16">헬로우 드림1</li>
+            <li class="w3-padding-16">블로그배포 2018 어워즈 수상! 크몽랭킹 1위! 카페/블로그 광고포스팅 팍팍 도와 드립니다</li>
+            <li class="w3-padding-16">
+              <i class="fa fa-star font-size-h6 color-dark-yellow-400" />
+              4.9
+              <h4 class="rate w3-show-inline-block">651개의 평가</h4>
+            </li>
+            <li class="w3-padding-16">
+              <h2>₩ 100,000</h2>
+            </li>
+            <li class="w3-light-grey w3-padding-24">
+              <button class="w3-button w3-teal w3-padding-large w3-hover-black">조회하기</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </slide>
   </carousel>
