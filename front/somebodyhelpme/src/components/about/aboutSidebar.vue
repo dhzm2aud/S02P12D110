ae8c09b58661f4efe42f0334b8f5459d7bb6bd22
이현우 <nyapy@naver.com>
2020-01-29 12:14:19 +0900

    S02P12D110-80 경매등록 ui 틀작성

--- front/somebodyhelpme/src/components/about/aboutSidebar.vue
+++ front/somebodyhelpme/src/components/about/aboutSidebar.vue
@@ -5,13 +5,13 @@
       onclick="w3_close()"
       class="w3-bar-item w3-button w3-padding w3-text-teal"
     >
-      <i class="fa fa-th-large fa-fw w3-margin-right"></i>ProfileModifiy
+      <i class="fa fa-th-large fa-fw w3-margin-right"></i>프로필 수정
     </a>
     <a href="#AuctionCancel" onclick="w3_close()" class="w3-bar-item w3-button w3-padding">
-      <i class="fa fa-user fa-fw w3-margin-right"></i>AuctionCancel
+      <i class="fa fa-user fa-fw w3-margin-right"></i>경매 등록
     </a>
     <a href="#AuctionRegist" onclick="w3_close()" class="w3-bar-item w3-button w3-padding">
-      <i class="fa fa-envelope fa-fw w3-margin-right"></i>AuctionRegist
+      <i class="fa fa-envelope fa-fw w3-margin-right"></i>경매 취소
     </a>
   </div>
 </template>
