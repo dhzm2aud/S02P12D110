27ca4e1d273cc477c0c6ec32f4be47f765dedc17
이현우 <nyapy@naver.com>
2020-01-30 12:32:46 +0900

    [test] data 삭제 기능 작동하는지

--- front/somebodyhelpme/src/components/about/AuctionCancel.vue
+++ front/somebodyhelpme/src/components/about/AuctionCancel.vue
@@ -23,30 +23,11 @@
 </template>
 
 <script>
+import data from "@/data/auctions.js";
 export default {
   data() {
     return {
-      auctions: [
-        { Title: "당근", Contents: "주황색", Time: "12:00", Time2: "20:00" },
-        {
-          Title: "딸기",
-          Contents: "빨간색 상큼한 맛",
-          Time: "9:00",
-          Time2: "19:00"
-        },
-        {
-          Title: "수박",
-          Contents: "초록색, 속은 빨간색, 달다",
-          Time: "10:00",
-          Time2: "18:00"
-        },
-        {
-          Title: "참외",
-          Contents: "노란색, 달다, 씨가 많아",
-          Time: "11:00",
-          Time2: "15:00"
-        }
-      ]
+      auctions: data
     };
   }
 };
