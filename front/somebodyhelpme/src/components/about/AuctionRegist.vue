d1c5b06d017afdcc0f1acd3953beb6c082087878
이현우 <nyapy@naver.com>
2020-01-30 15:17:59 +0900

    [test] data  등록기능 작동시험 issue : S02P12D110-12 story :S02P12D110-80

--- front/somebodyhelpme/src/components/about/AuctionRegist.vue
+++ front/somebodyhelpme/src/components/about/AuctionRegist.vue
@@ -1,58 +1,94 @@
 <template>
   <div id="AuctionRegist">
     <h2>경매 등록</h2>
-    <form action target="_blank">
-      <div class="w3-section">
-        <label>Deadline</label>
-        <input
-          class="w3-input w3-border"
-          type="datetime-local"
-          name="register-Deadline"
-          placeholder="Deadline"
-        />
-      </div>
-      <div class="w3-section">
-        <label>Title</label>
-        <input class="w3-input w3-border" type="text" name="register-title" placeholder="Title" />
-      </div>
 
-      <div class="w3-section">
-        <label>Contents</label>
-        <br />
-        <textarea
-          class="w3-input w3-border"
-          name="register-contents"
-          id="content"
-          rows="10"
-          placeholder="내용"
-        ></textarea>
-      </div>
-      <div class="w3-section">
-        <label>Details</label>
-        <input class="w3-input w3-border" type="file" name="register-file" placeholder="file" />
-      </div>
+    <div class="w3-section">
+      <label>Deadline</label>
+      <input
+        class="w3-input w3-border"
+        type="datetime-local"
+        name="register-Deadline"
+        placeholder="Deadline"
+      />
+    </div>
+    <div class="w3-section">
+      <label>Title</label>
+      <input
+        v-model="Title"
+        class="w3-input w3-border"
+        type="text"
+        name="register-title"
+        placeholder="Title"
+      />
+    </div>
 
-      <div class="w3-section">
-        <label>Closing hour</label>
-        <input
-          id="closingtime"
-          class="w3-input"
-          type="datetime-local"
-          name="Open"
-          style="width:40%"
-          required
-        />
-      </div>
-      <button type="submit" class="w3-button w3-black w3-margin-bottom">
-        <i class="fa fa-paper-plane w3-margin-right"></i> Register
-      </button>
-    </form>
+    <div class="w3-section">
+      <label>Contents</label>
+      <br />
+      <textarea
+        style="resize:none"
+        v-model="Contents"
+        class="w3-input w3-border"
+        name="register-contents"
+        id="content"
+        rows="10"
+        placeholder="내용"
+      ></textarea>
+    </div>
+    <div class="w3-section">
+      <label>Details</label>
+      <input class="w3-input w3-border" type="file" name="file" placeholder="file" />
+    </div>
+
+    <div class="w3-section">
+      <label>Closing hour</label>
+      <input
+        v-model="Time2"
+        id="closingtime"
+        class="w3-input"
+        type="datetime-local"
+        name="Open"
+        style="width:40%"
+        required
+      />
+    </div>
+    <button @click="regist" type="submit" class="w3-button w3-black w3-margin-bottom">
+      <i class="fa fa-paper-plane w3-margin-right"></i> Register
+    </button>
   </div>
 </template>
 
 <script>
+import data from "@/data/auctions.js";
 export default {
-  methods: {}
+  data() {
+    return {
+      Title: "",
+      Contents: "",
+      Time1: "",
+      Time2: "",
+      auctions: data,
+      member: {
+        memberId: "test1",
+        password: "asd",
+        name: "dd"
+      }
+    };
+  },
+  methods: {
+    regist() {
+      this.auctions.push({
+        Title: this.Title,
+        Contents: this.Contents,
+        Time1: this.Time1,
+        Time2: this.Time2
+      });
+      this.$router.push({
+        path: "#BackPage"
+      });
+    },
+    fileupload() {}
+  }
 };
 </script>
 
